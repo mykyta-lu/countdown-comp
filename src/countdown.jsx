280595e0210621d84e4b822679a61874f78d78e7
Mykyta <bidok24@gmail.com>
2024-03-26 07:12:26 -0400

    countdown moved to separate component

--- src/countdown.jsx
+++ src/countdown.jsx
@@ -1,10 +1,14 @@
-export default function calculateTimeLeft() {
+import { useEffect, useState } from "react";
+
+
+// need to change function to accept argument from main component
+function calculateTimeLeft() {
     let year = new Date().getFullYear();
-
+  
     const difference = +new Date(`10/01/${year}`) - +new Date();
-
+  
     let timeLeft = {};
-
+  
     if (difference > 0) {
         timeLeft = {
           days: Math.floor(difference / (1000 * 60 * 60 * 24)),
@@ -13,6 +17,37 @@ export default function calculateTimeLeft() {
           seconds: Math.floor((difference / 1000) % 60)
         };
       }
-
+  
       return timeLeft;
   }
+
+  const Countdown = () => {
+  
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+  
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setTimeLeft(calculateTimeLeft());
+      }, 1000);
+  
+      return () => clearTimeout(timer);
+    });
+  
+    const timerComponents = [];
+  
+    Object.keys(timeLeft).forEach((interval) => {
+      if (!timeLeft[interval]) {
+        return;
+      }
+  
+      timerComponents.push(<span>{timeLeft[interval]} {interval} {"  "}</span>);
+    });
+  
+    return (
+      <div>
+        {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+      </div>
+    );
+  }
+
+  export default Countdown;
